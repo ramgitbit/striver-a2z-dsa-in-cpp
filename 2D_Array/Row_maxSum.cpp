47c28b982977d0da6ef416377a946b9b3c2fbec8
ramgitbit <rambhawan111777@gmail.com>
2026-09-01 15:35:40 +0530

    Add programs to demonstrate 2D array functionalities including initialization, transpose, row sum, and total sum calculation

---
+++ 2D_Array/Row_maxSum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+    int sM = 0 , maxrow = -1;
+
+    // Print transpose of arr
+    for(int i = 0; i < 3; i++) {
+        // pahle row me ghus gaya 
+        int s = 0;
+        for(int j = 0; j < 4; j++) {
+            // ab row ke har element pe  jakr uska sum calculate karunaga
+            s += arr[i][j];
+        }
+        if (s>sM){
+            sM = s;
+            maxrow = i;
+        }
+
+    
+    }
+    cout<<maxrow<<" "<<sM;
+
+    return 0;
+}
